f81e1fef1335abeedbca10f98b48dd75f4e756c3
Neeraj Chaudhary <36655557+naspnrd@users.noreply.github.com>
2019-03-02 03:43:21 +0530

    Create String_reverse.py
    Some  of the tricks for string reversal

---
+++ String_reverse.py
@@ -0,0 +1,11 @@
+#string Reversal using slicing trick
+string = 'neerajChaudhary'
+
+##This is the best and fastest way to reverse the string
+##for i in string[::-1]:
+##    print(i)
+
+    
+##This is the best and fastest way to reverse the string
+
+##print("".join(reversed(string)))
